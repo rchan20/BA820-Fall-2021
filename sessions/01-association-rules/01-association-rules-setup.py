02d894996550573cd69059d6adfc07b4a648828a
rchan20 <87107649+rchan20@users.noreply.github.com>
2021-10-20 13:03:36 -0400

    Update 01-association-rules-setup.py

--- sessions/01-association-rules/01-association-rules-setup.py
+++ sessions/01-association-rules/01-association-rules-setup.py
@@ -11,7 +11,7 @@ import plotly.express as px    # interactive plotting for notebook environments
 from mlxtend.frequent_patterns import apriori, association_rules
 
 # my BILLING project in Google Cloud - replace "questrom" with your project
-PROJECT = "YOUR_PROJECT_HERE"
+#PROJECT = "YOUR_PROJECT_HERE"
 
 # NOTE: the dataset for module 1 is also in the Github repo, but this is only temporary
 # NOTE: you will need to configure GCP accounts and be able to query Big Query on your own!
@@ -19,3 +19,10 @@ PROJECT = "YOUR_PROJECT_HERE"
 # DATA Dictionary for the groceries dataset
 # tid = transaction id (id in a database that groups all items on a receipt together)
 # item = the individual item
+
+#SQL = """
+#select * from 'questrom.datasets.groceries'
+#"""
+
+groceries = pd.read_csv('groceries.csv')
+
